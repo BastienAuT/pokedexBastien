64ca585d56ce38a9f147de6bb65e139a0983a54f
dehcimal <dehcimal@localhost>
2023-04-13 18:36:29 +0200

    correction + avancée

--- src/components/Left/Left.jsx
+++ src/components/Left/Left.jsx
@@ -9,7 +9,7 @@ const Left = ({ setSelectedPokemon }) => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          "https://pokebuildapi.fr/api/v1/pokemon/generation/1"
+          "https://pokebuildapi.fr/api/v1/pokemon/generation/1?lang=fr"
         );
         setPokemonList(response.data);
       } catch (error) {
@@ -25,9 +25,34 @@ const Left = ({ setSelectedPokemon }) => {
     console.log("Selected Pokemon: ", pokemon);
   };
 
-  if (!pokemonList) {
-    return <div>Loading...</div>;
-  }
+  const typeColors = {
+    normal: "#A8A878",
+    feu: "#F08030",
+    combat: "#C03028",
+    eau: "#6890F0",
+    vol: "#A890F0",
+    plante: "#78C850",
+    poison: "#A040A0",
+    electrik: "#F8D030",
+    sol: "#E0C068",
+    psy: "#F85888",
+    roche: "#B8A038",
+    glace: "#98D8D8",
+    insecte: "#A8B820",
+    dragon: "#7038F8",
+    spectre: "#705898",
+    ténèbres: "#705848",
+    acier: "#B8B8D0",
+    fée: "#EE99AC",
+  };
+
+if (!pokemonList) {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+}
 
   return (
     <div className="left">
@@ -40,6 +65,19 @@ const Left = ({ setSelectedPokemon }) => {
           <div className="number">{pokemon.number}</div>
           <img className="image" src={pokemon.image} alt={pokemon.name} />
           <span className="name">{pokemon.name}</span>
+          <div className="types">
+            {pokemon.apiTypes.map((typeObj, index) => (
+              <span
+                key={index}
+                className={`type ${typeObj.name.toLowerCase()}`}
+                style={{
+                  backgroundColor: typeColors[typeObj.name.toLowerCase()],
+                }}
+              >
+                {typeObj.name}
+              </span>
+            ))}
+          </div>
         </div>
       ))}
     </div>
--- src/components/Right/Right.jsx
+++ src/components/Right/Right.jsx
@@ -5,6 +5,9 @@ import "./Right.scss";
 const Right = ({ selectedPokemon }) => {
   const [pokemonData, setPokemonData] = useState(null);
   const [pokemon, setPokemon] = useState(null);
+  const [evo, setEvo] = useState({});
+  const [hasEvolutions, setHasEvolutions] = useState(false);
+
 
   useEffect(() => {
     async function fetchData() {
@@ -21,6 +24,22 @@ const Right = ({ selectedPokemon }) => {
     fetchData();
   }, []);
 
+
+useEffect(() => {
+  const fetchEvoData = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.id + 1}`);
+      const data = await response.json();
+      const sprite = data.sprites.front_default;
+      setEvo({ ...data, sprite });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchEvoData();
+}, [selectedPokemon]);
+
+
   useEffect(() => {
     console.log("selectedPokemon:", selectedPokemon);
     console.log("pokemonData:", pokemonData);
@@ -33,7 +52,7 @@ const Right = ({ selectedPokemon }) => {
   }, [pokemonData, selectedPokemon]);
 
   if (!pokemon) {
-    return <div>Loading...</div>;
+    return <div className="select">Choisissez un pokemon de la liste afin d'avoir ses stats et son evolution...</div>;
   }
 
   const hp = pokemon.stats.HP;
@@ -48,21 +67,32 @@ const Right = ({ selectedPokemon }) => {
       <div className="pokemon">
         <span className="name">{pokemon.name}</span>
         <div className="number">{pokemon.number}</div>
-        <div className="picture">
-          <img className="bigPicture" src={pokemon.image} alt={pokemon.name} />
-        </div>
-
+        <img className="bigPicture" src={pokemon.image} alt={pokemon.name} />
         <div className="stats">
           <span>HP : {hp}</span>
           <span>ATTACK : {attack}</span>
           <span>DEFENSE : {defense}</span>
-          <span>ATTK. SPE{specialAttack}</span>
-          <span>DEF. SPE{specialDefense}</span>
-          <span>VITESSE{speed}</span>
-          <span>TOTAL{pokemon.stats.total}</span>
+          <span>ATTK. SPE : {specialAttack}</span>
+          <span>DEF. SPE : {specialDefense}</span>
+          <span>VITESSE : {speed}</span>
         </div>
       </div>
       <p>{pokemon.description}</p>
+      {pokemon.apiEvolutions && pokemon.apiEvolutions.length > 0 ? (
+        <div className="evolutions">
+          {evo.sprite && <img className="sprite" src={evo.sprite} alt={`${evo.name} sprite`} />}
+          {pokemon.apiEvolutions.map((evo) => (
+            <div key={evo.id}>
+              {evo.sprite && (
+                <img className="sprite" src={evo.sprite} alt={`${evo.name} sprite`} />
+              )}
+              <span>Evolue en : {evo.name}</span>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="no-evolutions">Ce pokemon n'a pas d'évolutions.</div>
+      )}
     </div>
   );
 };
